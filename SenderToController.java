92b9c026478a17172fd32154f83dc7bc7b4c481f
ranellran <115517307+ranellran@users.noreply.github.com>
2023-07-02 12:09:46 +0800

    Update SenderToController.java

--- SenderToController.java
+++ SenderToController.java
@@ -16,4 +16,50 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+public class GoogleSheetsExample {
+
+    private static final String APPLICATION_NAME = "Google Sheets Example";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String SPREADSHEET_ID = "your-spreadsheet-id";
+
+    public static void main(String[] args) {
+        try {
+            // Build a service object for interacting with the Sheets API
+            Sheets sheetsService = buildSheetsService();
+
+            // Example: SELECT statement
+            String selectQuery = "SELECT Name, Age FROM Sheet1";
+            List<List<Object>> selectResult = executeQuery(sheetsService, selectQuery);
+            printResult(selectResult);
+
+            // Example: UPDATE statement
+            String updateQuery = "UPDATE Sheet1 SET Age = '30' WHERE Name = 'John'";
+            int updateCount = executeUpdate(sheetsService, updateQuery);
+            System.out.println("Rows updated: " + updateCount);
+
+            // Example: DELETE statement
+            String deleteQuery = "DELETE FROM Sheet1 WHERE Age = '25'";
+            int deleteCount = executeUpdate(sheetsService, deleteQuery);
+            System.out.println("Rows deleted: " + deleteCount);
+
+            // Example: INSERT INTO statement
+            String insertQuery = "INSERT INTO Sheet1 (Name, Age) VALUES ('Emily', '35')";
+            int insertCount = executeUpdate(sheetsService, insertQuery);
+            System.out.println("Rows inserted: " + insertCount);
+
+            // Example: CREATE TABLE statement
+            String createTableQuery = "CREATE TABLE Sheet2 (ID, Name, Age)";
+            executeUpdate(sheetsService, createTableQuery);
+            System.out.println("Table created.");
+
+            // Example: DROP TABLE statement
+            String dropTableQuery = "DROP TABLE Sheet2";
+            executeUpdate(sheetsService, dropTableQuery);
+            System.out.println("Table dropped.");
+
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
 
